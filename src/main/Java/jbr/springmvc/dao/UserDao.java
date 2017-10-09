c392306113c9b08d99eeebbad4b609803f090116
vicalk <32147158+vicalk@users.noreply.github.com>
2017-10-09 22:58:06 +0300

    j

---
+++ src/main/Java/jbr/springmvc/dao/UserDao.java
@@ -0,0 +1,7 @@
+package jbr.springmvc.dao;
+import jbr.springmvc.model.Login;
+import jbr.springmvc.model.User;
+public interface UserDao {
+    void register(User user);
+    User validateUser(Login login);
+}
